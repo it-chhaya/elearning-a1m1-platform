b16f2ab977edb1b145152743d4a4a5d9482f88e7
Chan Chhaya <it.chhaya@gmail.com>
2026-08-06 11:08:33 +0700

    feat(instructor-profile-api): finished get instructor profile endpoint

---
+++ elearning-service/src/main/java/co/istad/chhaya/elearning/features/instructor/InstructorProfileController.java
@@ -0,0 +1,9 @@
+package co.istad.chhaya.elearning.features.instructor;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/instructor-profiles")
+public class InstructorProfileController {
+}
